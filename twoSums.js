f0675bb6e98d471579d42c674370a5f7df2b5ed0
AminulKazi <54209995+AminulKazi@users.noreply.github.com>
2019-09-03 20:32:04 +0100

    twoSums.js file
    This is the solution to the problem, it will console.log the result.

---
+++ twoSums.js
@@ -0,0 +1,17 @@
+var nums = [2, 7, 11, 15];
+var target = 22;
+myFunction(nums,target)
+
+function myFunction(nums, target) {
+  var foundIndexes = true;
+    for(var i = 0; i < nums.length; i++){
+      for(var j = 0; j < nums.length; j++){
+        if(nums[i] + nums[j] == target){
+          if(foundIndexes){
+          console.log(nums[i] + " " + nums[j]);
+            foundIndexes = false;
+        }
+        }
+      }
+      }
+    }
